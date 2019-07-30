85ec818e06c76489b597e81d67c9cda79f3e1339
liuxuanxin <liuxuanxin@bjfu.edu.cn>
2019-07-30 21:51:56 +0800

    update

--- preprocessing.py
+++ preprocessing.py
@@ -21,27 +21,42 @@ def load_data_from_mat(data_path, snr_index, test_split=0.3):
                 for i, typ in enumerate(['im', 're']):
                     path = os.path.join(data_path, 'src_{}_{}.mat'.format(s, typ))
                     dat[i, :, 0] = loadmat(path)['src_{}_{}'.format(s, typ)][snr_index]
-                train_ind = int(6000 / 128 * (1 - test_split))
-                for i in range(6000 / 128):
-                    if i > train_ind - 1:
-                        test_data.append(dat[:, i * 128:(i + 1) * 128, :])
-                        test_label.append(index)
-                    else:
-                        train_data.append(dat[:, i * 128:(i + 1) * 128, :])
-                        train_label.append(index)
+                split = int(6000 * (1 - test_split))
+                for i in range(split - 127):
+                    train_data.append(dat[:, i:i + 128, :])
+                    train_label.append(index)
+                for i in range((6000-split-128)/128):
+                    test_data.append(dat[:, split + i * 128:split + (i + 1) * 128, :])
+                    test_label.append(index)
+                # train_ind = int(6000 / 128 * (1 - test_split))
+                # for i in range(6000 / 128):
+                #     if i > train_ind - 1:
+                #         test_data.append(dat[:, i * 128:(i + 1) * 128, :])
+                #         test_label.append(index)
+                #     else:
+                #         train_data.append(dat[:, i * 128:(i + 1) * 128, :])
+                #         train_label.append(index)
+
         else:
             dat = np.zeros((2, 6000, 1))
             for i, typ in enumerate(['im', 're']):
                 path = os.path.join(data_path, 'src_{}_{}.mat'.format(src, typ))
                 dat[i, :, 0] = loadmat(path)['src_{}_{}'.format(src, typ)][snr_index]
-            train_ind = int(6000/128 * (1 - test_split))
-            for i in range(6000/128):
-                if i > train_ind - 1:
-                    test_data.append(dat[:, i * 128:(i + 1) * 128, :])
-                    test_label.append(index)
-                else:
-                    train_data.append(dat[:, i * 128:(i + 1) * 128, :])
-                    train_label.append(index)
+            split = int(6000 * (1 - test_split))
+            for i in range(split - 127):
+                train_data.append(dat[:, i:i + 128, :])
+                train_label.append(index)
+            for i in range((6000 - split - 128) / 128):
+                test_data.append(dat[:, split + i * 128:split + (i + 1) * 128, :])
+                test_label.append(index)
+            # train_ind = int(6000/128 * (1 - test_split))
+            # for i in range(6000/128):
+            #     if i > train_ind - 1:
+            #         test_data.append(dat[:, i * 128:(i + 1) * 128, :])
+            #         test_label.append(index)
+            #     else:
+            #         train_data.append(dat[:, i * 128:(i + 1) * 128, :])
+            #         train_label.append(index)
         print np.array(train_data).shape
     return np.array(train_data), np.array(train_label), np.array(test_data), np.array(test_label)
 
